89ad2e6cc27536be05f8410cbfe9df49f3204129
ml <miklakt@gmail.com>
2022-09-10 02:58:25 +0300

    minor cleanup

--- src/topology.hpp
+++ src/topology.hpp
@@ -12,12 +12,6 @@ T kappa(T const &N, T const &eta = T(1.0)){
 
 template <class T>
 T eta(int const &g, int const &q){
-    /* qq = 1+np.sum([q**i for i in range(1,g+1)])
-    if g == 1:
-        eta = math.atan(1/math.sqrt(q))*2/np.pi*qq
-    if g == 2:
-        eta =  math.atan(1/math.sqrt(q*(q+2)))*2/np.pi*qq
-    return eta */
     int qq = 1;
     for (int i=1; i<g+1; ++i){
         qq+=q*i;
@@ -32,7 +26,5 @@ T eta(int const &g, int const &q){
         throw std::invalid_argument("g other than 1 and 2 are not implemented");
     }
 }
-    
+
 } // namespace topology
-
-

33ad8746ed6d30572eb3e17f814f97f7916169c7
Mikhail Laktionov <miklakt@gmail.com>
2022-09-21 02:27:41 +0300

    threading c++ calls

--- scf_pb/__vectorization.py
+++ scf_pb/__vectorization.py
@@ -3,16 +3,16 @@ import inspect
 import itertools
 import numpy as np
 
-try:
-    import tqdm
-    __TRY_TQDM__ = lambda x, **kwargs: tqdm.tqdm(x, **kwargs)
-except:
-    __TRY_TQDM__ = lambda x, **kwargs: x
+from concurrent.futures import ThreadPoolExecutor
 
-def vectorize(product = True) -> Callable:
+def vectorize() -> Callable:
     def decorator(func : Callable) -> Callable:
         signature = inspect.signature(func)
         def wrapped(*args, **kwargs):
+
+            progressbar = kwargs.pop("progressbar", False)
+            max_workers = kwargs.pop("max_workers", 1)
+
             args_to_kwargs = {param : arg for param, arg in zip(signature.parameters, args)}
             all_args = dict(**args_to_kwargs, **kwargs)
             vector_args = {k : v for k, v in all_args.items() if isinstance(v, (list, np.ndarray))}
@@ -21,23 +21,30 @@ def vectorize(product = True) -> Callable:
                 return func(**all_args)
             scalar_args = {k : v for k, v in all_args.items() if k not in vector_args}
 
-            if product:
-                iteration = itertools.product(*vector_args.values())
-            #else:
-            #    iteration = zip(*vector_args.values())
+            iteration = itertools.product(*vector_args.values())
 
+            pool_results = []
+            with ThreadPoolExecutor(max_workers=max_workers) as pool:
+                for it in iteration:
+                    iteration_args = {k : v for k, v  in zip(vector_args, it)}
+                    iteration_args.update(scalar_args)
+                    #results[flatiter.coords] = func(**iteration_args)
+                    pool_results.append(pool.submit(func, **iteration_args))
+                    #next(flatiter)
+            
             results = np.empty(shape = shape, dtype = object)
             flatiter = results.flat
-            for it in __TRY_TQDM__(iteration, total = np.product(shape)):
-                iteration_args = {k : v for k, v  in zip(vector_args, it)}
-                iteration_args.update(scalar_args)
-                results[flatiter.coords] = func(**iteration_args)
+            
+            if progressbar:
+                import tqdm
+                pool_results = tqdm.tqdm(pool_results)
+            
+            for pool_result in pool_results:
+                results[flatiter.coords] = pool_result.result()
                 next(flatiter)
-                #results.append(result)
-            #results = np.reshape(results, shape)
-            #if not shape:
-            #    results = results.item()
+            
             return results
+
         wrapped.__signature__ = signature
         wrapped.__name__ = func.__name__
         wrapped.__module__ = func.__module__

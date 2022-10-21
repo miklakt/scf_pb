4f1916fe99e73200a618588b37a5459e22b15df8
ml <miklakt@gmail.com>
2022-10-21 15:08:40 +0400

    mobility factor exposed

--- src/pybind.cpp
+++ src/pybind.cpp
@@ -123,4 +123,6 @@ PYBIND11_MODULE(_scf_pb, m){
     m.def("D_eff_external", &D_eff_external, py::call_guard<py::gil_scoped_release>(), "Effective diffusion coefficient through polymer brush membrane (cxx)", py::kw_only{},  "phi"_a, "chi"_a, "chi_PC"_a, "a0"_a, "a1"_a, "particle_width"_a, "particle_height"_a, "k_smooth"_a, "a"_a, "b"_a);
 
     m.def("PC", &PC_open, py::call_guard<py::gil_scoped_release>(), "Partition coefficient of particles in polymer brush and a semi-infinite solution (cxx)",  "N"_a, "sigma"_a, "chi"_a, py::kw_only{}, "chi_PC"_a, "a0"_a, "a1"_a, "particle_width"_a, "particle_height"_a);
+
+    m.def("mobility_factor", &particle_mobility::mobility_factor, py::call_guard<py::gil_scoped_release>(), "Polymer network mobility factor", "phi"_a, "d"_a, "k_smooth"_a);
 }

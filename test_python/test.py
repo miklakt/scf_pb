223578b67e1d2c0dbb9088880e2938fa478370f7
ml <miklakt@gmail.com>
2022-10-21 14:38:10 +0400

    minor

--- test_python/test.py
+++ test_python/test.py
@@ -10,7 +10,7 @@ import time
 #16 workers 9.385826349258423 seconds
 # access to cache 0.029059171676635742 seconds
 start_time = time.time()
-D_eff = scf_pb.D_eff_v(N=1000, sigma = 0.02, chi = np.linspace(0, 1, 10), chi_PC = np.linspace(0, 1, 50), a0=0.18, a1=-0.09, particle_width=4, particle_height = 4, k_smooth = 4, progressbar = True, max_workers = 4)
+D_eff = scf_pb.D_eff_v(N=1000, sigma = 0.02, chi = np.linspace(0, 1, 10), chi_PC = np.linspace(0, 1, 50), a0=0.18, a1=-0.09, particle_width=4, particle_height = 4, k_smooth = 4, progressbar = True)
 print("--- %s seconds ---" % (time.time() - start_time))
 # %%
 scf_pb.phi(N=1000, chi=0.3, z=0, sigma = 0.02)

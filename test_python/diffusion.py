82f526a3a3a07a0a105d5040883558007b5c26f1
ml <miklakt@gmail.com>
2022-12-03 04:57:56 +0400

    back to Sphere

--- test_python/diffusion.py
+++ test_python/diffusion.py
@@ -4,21 +4,24 @@ import numpy as np
 import scf_pb
 
 # %%
-d = 2
+d = 10
 a0 = 0.18
 a1 = -0.09
-chi = 0.0
-sigma = 0.08
-N = 2000
+chi = 0.6
+chi_PC =-1
+sigma = 0.02
+N = 1000
 
 H = scf_pb.D(N=N, sigma= sigma, chi = chi)
 z = np.linspace(0, H*1.5)
 #%%
-phi = scf_pb.phi_v(N=N, sigma=sigma, chi = chi, z=z)
+phi = scf_pb.phi_v(N=N, sigma=sigma, chi = chi, z=z).astype(float)
 plt.plot(z, phi)
 # %%
 Pi = scf_pb.Pi_v(N=N, sigma=sigma, chi = chi, z=z)
+Pi2 = -np.log(1-phi) - phi - chi*phi**2
 plt.plot(z, Pi)
+plt.plot(z, Pi2)
 #%%
 def volume(w, h = None):
     if h is None:
@@ -48,13 +51,7 @@ def volume_free_energy(phi, chi_PS, w, h=None):
 def free_energy_penalty_phi(phi, a1, a2, chi_PS, chi_PC, w, h=None):
     return surface_free_energy(phi, a1, a2, chi_PS, chi_PC, w, h)+volume_free_energy(phi, chi_PS, w, h)
 #%%
-chi_PC =-2
-#chi_ads = chi_PC - chi*(1-phi)
-#chi_crit = 6*np.log(5/6)
-#gamma = (chi_ads-chi_crit)*(a0*phi+a1*phi**2)
 fe_approx = [free_energy_penalty_phi(phi_, a0, a1, chi, chi_PC, d) for phi_ in phi]
-#fe_approx =  4/3*np.pi*(d/2)**3*Pi + 4*np.pi*(d/2)**2*gamma
-#fe_approx =  np.pi*d**3/4*Pi +3/2*np.pi*d**2*gamma
 fe = scf_pb.free_energy_v(
     N=N, sigma=sigma, chi=chi,
     chi_PC = chi_PC,
